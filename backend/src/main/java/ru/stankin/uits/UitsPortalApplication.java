cdff596b1f99c019f3858ed9a3678d69e999a34e
Tim Ter <timter32@gmail.com>
2026-02-21 16:09:39 +0300

    refactor: rename main class and change directory

--- backend/src/main/java/ru/stankin/uits/UitsPortalApplication.java
+++ backend/src/main/java/ru/stankin/uits/UitsPortalApplication.java
@@ -1,13 +1,13 @@
-package ru.stankin;
+package ru.stankin.uits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UitsApplication {
+public class UitsPortalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UitsApplication.class, args);
+		SpringApplication.run(UitsPortalApplication.class, args);
 	}
 
 }
