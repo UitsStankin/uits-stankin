b99ac5bdd088317efba9a848103fc66c13515485
Tim Ter <timter32@gmail.com>
2026-02-23 00:31:35 +0300

    feat: add CustomUserDetailsService to load users from DB

---
+++ backend/src/main/java/ru/stankin/uits/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package ru.stankin.uits.security;
+
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.stankin.uits.module.user.entity.User;
+import ru.stankin.uits.module.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return new SecurityUser(user);
+    }
+}
