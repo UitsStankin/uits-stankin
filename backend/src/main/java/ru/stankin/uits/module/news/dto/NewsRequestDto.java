db5195d35fc47b2f3fdcc75525ae71f127172f4c
Tim Ter <timter32@gmail.com>
2026-02-23 12:17:54 +0300

    feat: add DTO for News

---
+++ backend/src/main/java/ru/stankin/uits/module/news/dto/NewsRequestDto.java
@@ -0,0 +1,32 @@
+package ru.stankin.uits.module.news.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewsRequestDto {
+    @NotBlank(message = "Title cant be empty")
+    @Size(min = 5, max = 100, message = "Title must be from 5 to 100 symbols")
+    String title;
+
+    @Size(max = 225, message = "Short description is too long")
+    String shortDescription;
+
+    @NotBlank(message = "Post type required")
+    String postType;
+
+    String previewImage;
+    String previewImageDescription;
+
+    @NotBlank(message = "Content cant be empty")
+    String content;
+
+    boolean display;
+}
---
+++ backend/src/main/java/ru/stankin/uits/module/news/dto/NewsResponseDto.java
@@ -0,0 +1,23 @@
+package ru.stankin.uits.module.news.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewsResponseDto {
+    Long id;
+    String title;
+    String shortDescription;
+    String postType;
+    String previewImage;
+    String content;
+    OffsetDateTime createdAt;
+    String authorName;
+}
