a5f057e9c2450f68d3429cdaeeb01d993f64424a
Vladimir Devyatov <devyatov2704@icloud.com>
2026-03-06 20:08:36 +0300

    add: example routes

---
+++ frontend/src/routes/protectedRoute.tsx
@@ -0,0 +1,18 @@
+// import { Navigate, useLocation } from 'react-router-dom';
+// import { useAuth } from '../features/auth/lib/useAuth'; // Ваш хук проверки авторизации
+
+// interface ProtectedRouteProps {
+//   children: React.ReactNode;
+// }
+
+// export default function ProtectedRoute({ children }: ProtectedRouteProps) {
+//   const { isAuthenticated } = useAuth();
+//   const location = useLocation();
+
+//   if (!isAuthenticated) {
+//     // Редирект на логин с сохранением текущего пути
+//     return <Navigate to="/auth/login" state={{ from: location }} replace />;
+//   }
+
+//   return <>{children}</>;
+// }
