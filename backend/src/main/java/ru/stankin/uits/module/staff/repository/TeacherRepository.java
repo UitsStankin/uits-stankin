2d08d57682b47dad954e7a8ec64fc2a3897e4814
eeffeecct <timter32@gmail.com>
2026-07-27 20:22:24 +0400

    chore: @NullMarked в TeacherRepository для согласования с контрактом Spring Data

--- backend/src/main/java/ru/stankin/uits/module/staff/repository/TeacherRepository.java
+++ backend/src/main/java/ru/stankin/uits/module/staff/repository/TeacherRepository.java
@@ -1,5 +1,6 @@
 package ru.stankin.uits.module.staff.repository;
 
+import org.jspecify.annotations.NullMarked;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -8,6 +9,7 @@ import ru.stankin.uits.module.staff.entity.Teacher;
 
 import java.util.Optional;
 
+@NullMarked
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
     Optional<Teacher> findByUserId(Long userId);
 
