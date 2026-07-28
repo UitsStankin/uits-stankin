aae9fe11dad9474998adabec33dd78434296cf01
eeffeecct <timter32@gmail.com>
2026-07-28 20:03:06 +0400

    refactor: удалены неиспользуемые методы репозиториев

--- backend/src/main/java/ru/stankin/uits/module/user/repository/UserRepository.java
+++ backend/src/main/java/ru/stankin/uits/module/user/repository/UserRepository.java
@@ -11,8 +11,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
-    boolean existsByUsername(String username);
-
     @Modifying
     @Query("update User u set u.lastLogin = :now where u.id = :id")
     void updateLastLogin(Long id, OffsetDateTime now);
