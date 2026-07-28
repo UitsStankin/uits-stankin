2498a9cde8f3fb6af77a29cc229d980278139077
eeffeecct <timter32@gmail.com>
2026-07-28 18:06:32 +0400

    feat: выборка автора новости одним запросом через @EntityGraph

--- backend/src/main/java/ru/stankin/uits/module/news/repository/NewsRepository.java
+++ backend/src/main/java/ru/stankin/uits/module/news/repository/NewsRepository.java
@@ -2,9 +2,11 @@ package ru.stankin.uits.module.news.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.stankin.uits.module.news.entity.NewsPost;
 
 public interface NewsRepository extends JpaRepository<NewsPost, Long> {
+    @EntityGraph(attributePaths = {"author"})
     Page<NewsPost> findAllByDisplayTrue(Pageable pageable);
 }
